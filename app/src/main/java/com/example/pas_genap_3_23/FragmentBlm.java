c0c2b0d6d6c51f56b8dc649c9996f28e7b712e70
AgustinusGalihGumilang <agustinus9141@gmail.com>
2025-05-22 10:35:55 +0700

    feat[AGG] ui

---
+++ app/src/main/java/com/example/pas_genap_3_23/FragmentBlm.java
@@ -0,0 +1,17 @@
+package com.example.pas_genap_3_23;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentBlm extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blm, container, false);
+
+        return view;
+    }
+}
