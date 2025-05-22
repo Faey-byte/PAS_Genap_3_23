3ea6006240b21ca9d3ab8bff604b03f39737194c
Muhammad Al Faiz <reyhankunlah@gmail.com>
2025-05-22 12:40:20 +0700

    feat[MAF] fix endpoint

--- app/src/main/java/com/example/pas_genap_3_23/PlayerResponse.java
+++ app/src/main/java/com/example/pas_genap_3_23/PlayerResponse.java
@@ -4,6 +4,6 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class PlayerResponse {@SerializedName("players")
-public List<Player> players;
+public class PlayerResponse {@SerializedName("player")
+public List<Player> player;
 }
